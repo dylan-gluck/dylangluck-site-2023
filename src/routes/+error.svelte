b658d1cda520c5fc027d6eaef4d7552eee3aa824
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2023-05-23 15:24:44 -0700

    Adds error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<div class="grid h-screen place-items-center">
+	<section class="p-10 mx-auto max-w-7xl">
+		<div class="w-full mx-auto md:w-1/2 lg:w-1/3">
+			<p class="mt-5 mb-3 text-xl font-bold text-primary md:text-2xl">Page not found (404)</p>
+			<p class="mb-3 text-base font-medium text-base-content">
+				The page you're looking for may have moved or no longer exists. Head back to the <a
+					href="/"
+					class="link">homepage</a
+				>, or one of the links above.
+			</p>
+		</div>
+	</section>
+</div>
