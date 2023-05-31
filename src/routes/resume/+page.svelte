474fe5688b02081bcb4fdca3199bda172055e273
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2023-05-31 12:14:56 -0700

    Updated resume + adds animations

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { onMount } from 'svelte';
-	import { beforeNavigate } from '$app/navigation';
 	import { slide } from 'svelte/transition';
 	import { backOut } from 'svelte/easing';
 	import Navbar from '$lib/components/Navbar.svelte';
@@ -9,10 +8,6 @@
 
 	onMount(() => {
 		animate = true;
-	});
-
-	beforeNavigate(() => {
-		animate = false;
 	});
 </script>
 
@@ -47,47 +42,56 @@
 					<div
 						class="space-y-12 relative sm:px-4 sm:-ml-4 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-700"
 					>
-						<div in:slide={{ delay: 200, easing: backOut }} class="flex flex-col sm:relative">
+						<div class="flex flex-col sm:relative">
 							<div
+								in:slide={{ axis: 'x', delay: 300, duration: 200, easing: backOut }}
 								class="sm:absolute sm:top-2 sm:w-4 sm:h-4 sm:rounded-full sm:left-[-35px] sm:z-[1] bg-primary"
 							/>
-							<p class="text-xs tracking-wide uppercase">Unqork</p>
-							<h3 class="text-xl font-semibold tracking-wide">Engineering Manager</h3>
-							<time class="text-xs tracking-wide uppercase dark:text-gray-400"
-								>2019 &mdash; Present</time
-							>
-							<p class="max-w-xl mt-3">
-								Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum et
-								malesuada fames ac ante ipsum primis in faucibus.
-							</p>
+							<div in:slide={{ delay: 200, easing: backOut }}>
+								<p class="text-xs tracking-wide uppercase">Unqork</p>
+								<h3 class="text-xl font-semibold tracking-wide">Engineering Manager</h3>
+								<time class="text-xs tracking-wide uppercase dark:text-gray-400"
+									>2019 &mdash; Present</time
+								>
+								<p class="max-w-xl mt-3">
+									Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum
+									et malesuada fames ac ante ipsum primis in faucibus.
+								</p>
+							</div>
 						</div>
-						<div in:slide={{ delay: 400, easing: backOut }} class="flex flex-col sm:relative">
+						<div class="flex flex-col sm:relative">
 							<div
+								in:slide={{ axis: 'x', delay: 400, duration: 200, easing: backOut }}
 								class="sm:absolute sm:top-2 sm:w-4 sm:h-4 sm:rounded-full sm:left-[-35px] sm:z-[1] bg-primary"
 							/>
-							<p class="text-xs tracking-wide uppercase">Aaptiv</p>
-							<h3 class="text-xl font-semibold tracking-wide">UX Specialist</h3>
-							<time class="text-xs tracking-wide uppercase dark:text-gray-400"
-								>2018 &mdash; 2019</time
-							>
-							<p class="max-w-xl mt-3">
-								Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum et
-								malesuada fames ac ante ipsum primis in faucibus.
-							</p>
+							<div in:slide={{ delay: 300, easing: backOut }}>
+								<p class="text-xs tracking-wide uppercase">Unqork</p>
+								<h3 class="text-xl font-semibold tracking-wide">Engineering Manager</h3>
+								<time class="text-xs tracking-wide uppercase dark:text-gray-400"
+									>2019 &mdash; Present</time
+								>
+								<p class="max-w-xl mt-3">
+									Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum
+									et malesuada fames ac ante ipsum primis in faucibus.
+								</p>
+							</div>
 						</div>
-						<div in:slide={{ delay: 600, easing: backOut }} class="flex flex-col sm:relative">
+						<div class="flex flex-col sm:relative">
 							<div
+								in:slide={{ axis: 'x', delay: 500, duration: 200, easing: backOut }}
 								class="sm:absolute sm:top-2 sm:w-4 sm:h-4 sm:rounded-full sm:left-[-35px] sm:z-[1] bg-primary"
 							/>
-							<p class="text-xs tracking-wide uppercase">Unqork</p>
-							<h3 class="text-xl font-semibold tracking-wide">Engineering Manager</h3>
-							<time class="text-xs tracking-wide uppercase dark:text-gray-400"
-								>2019 &mdash; Present</time
-							>
-							<p class="max-w-xl mt-3">
-								Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum et
-								malesuada fames ac ante ipsum primis in faucibus.
-							</p>
+							<div in:slide={{ delay: 400, easing: backOut }}>
+								<p class="text-xs tracking-wide uppercase">Unqork</p>
+								<h3 class="text-xl font-semibold tracking-wide">Engineering Manager</h3>
+								<time class="text-xs tracking-wide uppercase dark:text-gray-400"
+									>2019 &mdash; Present</time
+								>
+								<p class="max-w-xl mt-3">
+									Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum
+									et malesuada fames ac ante ipsum primis in faucibus.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
