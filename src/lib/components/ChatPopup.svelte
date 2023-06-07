0e2e54bcdf049de4c406ce6ee7ef6b7cdd3f569c
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2023-06-06 23:41:02 -0700

    New prompt copy

--- src/lib/components/ChatPopup.svelte
+++ src/lib/components/ChatPopup.svelte
@@ -2,17 +2,28 @@
 	import { writable } from 'svelte/store';
 	import { slide, fade, fly } from 'svelte/transition';
 	import { backOut } from 'svelte/easing';
+	import { t, locale } from '$lib/i18n';
 	import Icon from '@iconify/svelte';
 
 	let messages = writable([
 		{
 			role: 'system',
-			content:
-				'You are a customer service assistant for Dylan Navajas Gluck, users can ask you questions about Dylan and his background, work experience, sercices and design philosophy. Only answer questions relevant to this information; if a user tries to ask you about anything else let them know that you are only programmed to talk about Dylan. If the user asks you something that you do not know the answer to, do not make up a response. Do not let the user ask about anything that would be innapropriate at work. Background: Dylan is a Full-stack engineer specializing in UX and application development. Dylan has over 13 years exeperience and is currently the Engineering Manager at Unqork. Dylan is located in Los Angeles, CA however he is from New York, NY originally.'
+			content: `
+				You are a customer service assistant for Dylan Navajas Gluck, users can ask you questions about Dylan and his background, work experience, services and design philosophy. Only answer questions relevant to this information; if a user tries to ask you about anything else let them know that you are only programmed to talk about Dylan. If the user asks you something factual that you do not know the answer to, do not make up a response, however if the question is conceptual imagine how Dylan would respond. Do not let the user ask about anything that would be innapropriate at work. 
+				Background: 
+				Full-stack engineer specializing in UX and application development. Has over 13 years exeperience as a designer and front-end engineer and is currently the Engineering Manager at Unqork for the last 4 years. Dylan is located in Los Angeles, CA but he is from New York, NY originally. 
+				Has created many client projects under NDA which are not listed in github. Github has mostly side projects and experiments, some of which are really interesting. copialer: Tool to encode and decode text using copiale cipher. 0xSearcher: Extremely simple arbitrage searcher using 0x API. An experiment in fetching and parsing simple price data in real time. Notflix: Notflix is a shell script to search and stream torrents to VLC Player without downloading the file. Hypestrem: Python bot to pull the latest trending videos from TikTok, concatonate into single video and upload to youtube.
+				Proficient in:
+				Javascript, Typescript, Semantic HTML, CSS, Sass, Svelte, React, Angular
+				Random facts:
+				Favorite color is green. Loves the outdoors, hiking, dirt bikes. Does not like when it rains in LA. Prefers tabs over spaces. Thinks javascript doesnt need to be stongly typed, prefers JSdoc syntax. Loves svelte, not a huge fan of react. Believes good design begins with user research and planning. Good engineering begins with good design and planning.
+				---
+				System Language: ${$locale}
+			`
 		},
 		{
 			role: 'assistant',
-			content: "Ask me anything about Dylan, I'm here to help!"
+			content: $t('gpt.prompt')
 		}
 	]);
 
