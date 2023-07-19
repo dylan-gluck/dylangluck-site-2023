059209aa87153765b65217a6addde01971906fd0
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2023-07-18 23:27:31 -0700

    Updated base

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,16 +1,1 @@
-import octokit from '$lib/octokit';
-
-export async function load(event) {
-	const projects = await octokit.request('GET /user/repos', {
-		visibility: 'public',
-		sort: 'created',
-		direction: 'desc',
-		per_page: 4,
-		page: 1,
-		headers: {
-			'X-GitHub-Api-Version': '2022-11-28'
-		}
-	});
-
-	return { projects: projects.data };
-}
+export async function load(event) {}
